1f269d7ab95ab9322bf6bf8d2a7e634546fee055
Derek <peter890701@gmail.com>
2021-11-22 00:59:03 +0800

    feat : add TestVerify

--- src/tools/otp/otp_test.go
+++ src/tools/otp/otp_test.go
@@ -24,8 +24,8 @@ func TestNewGoogleOTP(t *testing.T) {
 			name: "TestGenerate",
 			args: args{
 				key:         xid.New().String(),
-				issuer:      "test",
-				accountName: "test",
+				issuer:      "BitoPro",
+				accountName: "peter890701@gmail.com",
 				digits:      6,
 			},
 		},
@@ -40,6 +40,44 @@ func TestNewGoogleOTP(t *testing.T) {
 			assert.Equal(t, got.Digits, tt.args.digits)
 			assert.NotEmpty(t, got.Secret)
 			assert.NotEmpty(t, got.OtpAuth)
+		})
+	}
+}
+
+func TestVerify(t *testing.T) {
+	type args struct {
+		secret string
+		code   string
+		period uint
+		digits int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "TestVerify",
+			args: args{
+				secret: "MM3GIN3INJTWQZRRG5WTQ2TJNBSW2Z3H",
+				code:   "018633",
+				period: 30,
+				digits: 6,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Verify(tt.args.secret, tt.args.code, tt.args.period, tt.args.digits)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Verify() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Verify() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
